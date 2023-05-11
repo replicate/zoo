2537c407cbcec24fe312b533db359f78cfcc0e73
Charlie Holtz <charlieholtz@gmail.com>
2023-05-11 17:09:16 -0400

    reverse history

--- pages/index.js
+++ pages/index.js
@@ -461,7 +461,7 @@ export default function Home() {
             role="list"
             className="mt-12 grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-3 sm:gap-x-6 lg:grid-cols-4 xl:gap-x-8"
           >
-            {history.map((prediction) => (
+            {history.reverse().map((prediction) => (
               <li key={prediction.id} className="relative">
                 <div className="group aspect-h-7 aspect-w-10 block w-full overflow-hidden rounded-lg bg-gray-100 focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 focus-within:ring-offset-gray-100">
                   <img
