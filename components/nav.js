80f1f739917b78c218fe8c12dc8ed3b6e2342aa4
Charlie Holtz <charlieholtz@gmail.com>
2023-05-16 13:29:34 -0400

    revert link change

--- components/nav.js
+++ components/nav.js
@@ -24,11 +24,11 @@ export default function Nav() {
   return (
     <nav className="p-5 border-t-4 border-t-brand">
       <div className="flex">
-        <a href="/">
+        <Link href="/">
           <div className="hover:animate-spin flex-shrink-0 mb-0 mr-4">
             <span className="text-4xl">🦓</span>
           </div>
-        </a>
+        </Link>
         <div className="flex w-full justify-between">
           <div className="sm:flex text-lg items-center font-bold justify-center">
             <Link className="hover:underline text-sm sm:text-lg " href="/">
