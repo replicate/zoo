d0218ab137d6bc4e9b73f4e16b55a9339ce6b75f
Charlie Holtz <charlieholtz@gmail.com>
2023-05-16 17:17:22 -0400

    remove top margin

--- pages/memories.js
+++ pages/memories.js
@@ -42,7 +42,7 @@ export default function History() {
         ></link>
       </Head>
       <>
-        <div className="mt-6 md:flex md:items-center md:justify-between">
+        <div className="md:flex md:items-center md:justify-between">
           <div className="min-w-0 flex-1">
             <h2 className="text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight">
               Memories at the Zoo
