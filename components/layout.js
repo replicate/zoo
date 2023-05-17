679ed19c22aa37246deda0cf5d609a558251592d
Zeke Sikelianos <zeke@sikelianos.com>
2023-05-16 17:03:52 -0700

    Revert "add logomarks with tooltips"

--- components/layout.js
+++ components/layout.js
@@ -1,12 +1,10 @@
 import Nav from "./nav";
-import Footer from "./footer";
 
 export default function Layout({ children }) {
   return (
     <>
       <Nav />
       <main>{children}</main>
-      <Footer />
     </>
   );
 }
