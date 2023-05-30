df3eb9f5e7bc0e18cb57e41697d5a52a14a47a54
Charlie Holtz <charlieholtz@gmail.com>
2023-05-30 14:20:23 -0400

    show same pills on mobile

--- components/pills.js
+++ components/pills.js
@@ -20,23 +20,7 @@ export default function Pills() {
   }, []);
   return (
     <div>
-      <div className="sm:hidden">
-        <label htmlFor="tabs" className="sr-only">
-          Select a tab
-        </label>
-        {/* Use an "onChange" listener to redirect the user to the selected tab URL. */}
-        <select
-          id="tabs"
-          name="tabs"
-          className="block w-full rounded-md border-gray-300 focus:border-indigo-500 focus:ring-indigo-500"
-          defaultValue={tabs.find((tab) => tab.current).name}
-        >
-          {tabs.map((tab) => (
-            <option key={tab.name}>{tab.name}</option>
-          ))}
-        </select>
-      </div>
-      <div className="hidden sm:block">
+      <div className="block">
         <nav className="flex space-x-4" aria-label="Tabs">
           {tabs.map((tab) => (
             <a
