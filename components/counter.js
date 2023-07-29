54df70710248d51c993f788e771d4e1cc67eb76f
viruop <virajrai706@gmail.com>
2023-07-29 13:30:20 +0530

    fixed counter when tab is hidden

--- components/counter.js
+++ components/counter.js
@@ -4,12 +4,43 @@ const Counter = () => {
   const [tenthSeconds, setTenthSeconds] = useState(0);
 
   useEffect(() => {
-    const intervalId = setInterval(() => {
+    let intervalId;
+    let hidden = "";
+    let visibilityChange = "";
+
+    if (typeof document.hidden !== "undefined") {
+      hidden = "hidden";
+      visibilityChange = "visibilitychange";
+    } else if (typeof document.msHidden !== "undefined") {
+      hidden = "msHidden";
+      visibilityChange = "msvisibilitychange";
+    } else if (typeof document.webkitHidden !== "undefined") {
+      hidden = "webkitHidden";
+      visibilityChange = "webkitvisibilitychange";
+    }
+
+    const handleVisibilityChange = () => {
+      if (document[hidden]) {
+        // Tab is hidden, so start the timer
+        intervalId = setInterval(() => {
+          setTenthSeconds((tenthSeconds) => tenthSeconds + 1);
+        }, 100);
+      }
+    };
+
+    // Add the event listener for visibility changes
+    document.addEventListener(visibilityChange, handleVisibilityChange);
+
+    // Start the timer initially when the component mounts
+    intervalId = setInterval(() => {
       setTenthSeconds((tenthSeconds) => tenthSeconds + 1);
     }, 100); // now the interval is 100ms, so it increases every tenth of a second
 
-    // cleanup function to clear the interval when the component unmounts
-    return () => clearInterval(intervalId);
+    // cleanup function to clear the interval and remove the event listner when the component unmounts
+    return () => {
+      clearInterval(intervalId);
+      document.removeEventListener(visibilityChange, handleVisibilityChange);
+    };
   }, []); // passing an empty array as the second argument to useEffect makes it run only on mount and unmount
 
   return (
